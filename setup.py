5ff38dca0c20489fd82f040b25929d52fdfff711
KulichenkoV.S._KVS <cosmosx@mail.ru>
2026-02-22 17:17:28 +0300

    Initial commit: project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+﻿from setuptools import setup, find_packages
+
+setup(
+name="sokrat",
+version="0.1.0",
+packages=find_packages(where="src"),
+package_dir={"": "src"},
+install_requires=[],
+author="[Твоё имя]",
+description="[Краткое описание]",
+license="MIT",
+)
---
+++ src/main.py
@@ -0,0 +1,13 @@
+﻿"""
+Sokrat - главный модуль
+"""
+
+def main():
+    print("="*50)
+    print(" Sokrat v0.1.0")
+    print("="*50)
+    print(" Проект инициализирован")
+    return 0
+
+if __name__ == "__main__":
+    exit(main())
