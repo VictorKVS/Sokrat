d1b5f2e9efb8e4bbf5ee9d3898ba3288ddc403ec
KulichenkoV.S._KVS <cosmosx@mail.ru>
2026-02-22 17:18:18 +0300

    Initial commit

--- src/main.py
+++ src/main.py
@@ -1,13 +1,5 @@
-﻿"""
-Sokrat - главный модуль
-"""
-
-def main():
-    print("="*50)
-    print(" Sokrat v0.1.0")
-    print("="*50)
-    print(" Проект инициализирован")
-    return 0
+﻿def main():
+    print("Sokrat v0.1.0")
 
 if __name__ == "__main__":
-    exit(main())
+    main()
